2b8fc8976b9cd460874c0b7a065ef406bc525538
Maggie Zhang <maggiejz@google.com>
2021-12-22 14:31:35 -0600

    Added accepted file types

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ destination_uri = f"{gcs_output_uri}/{gcs_output_uri_prefix}/"
 name = f"projects/{project_id}/locations/{location}/processors/{processor_id}"
 dataset_name = 'expense_parser_results'
 table_name = 'doc_ai_extracted_entities'
+accepted_file_types = ["application/pdf","image/jpg","image/png","image/gif","image/tiff","image/jpeg","image/tif","image/webp","image/bmp"]
 
 # Create GCP clients
 docai_client = documentai.DocumentProcessorServiceClient()
@@ -71,14 +72,13 @@ def write_to_bq(dataset_name, table_name, entities_extracted_dict):
  
    job = bq_client.load_table_from_json(json_object, table_ref, job_config=job_config)
    error = job.result()  # Waits for table load to complete.
-   print("Load BQ table from JSON result: ", error)
+   
   
  
 def process_receipt(event, context):
    gcs_input_uri = 'gs://' + event['bucket'] + '/' + event['name']
 
-   # TODO: check the file types supported by expense parser
-   if(event['contentType'] == 'image/gif' or event['contentType'] == 'application/pdf' or event['contentType'] == 'image/tiff' ):
+   if event['contentType'] in accepted_file_types:
        input_config = documentai.types.document_processor_service.BatchProcessRequest.BatchInputConfig(gcs_source=gcs_input_uri, mime_type=event['contentType'])
        # Where to write results
        output_config = documentai.types.document_processor_service.BatchProcessRequest.BatchOutputConfig(gcs_destination=destination_uri)
@@ -107,14 +107,13 @@ def process_receipt(event, context):
        for blob in blob_list:
            # Download the contents of this blob as a bytes object.
            if ".json" not in blob.name:
-               print("Blob name: " + blob.name)
-               print(f"Skipping non-supported file type: {blob.name}")
+                print(f"Skipping non-supported file type: {blob.name}")
            else:
                 #Setting the output file name based on the input file name
                 print("Fetching from: " + blob.name)
                 #start = blob.name.rfind("/") + 1
                 #end = blob.name.rfind(".") + 1           
-                #input_filename = blob.name[start:end:] + "gif" #TODO: why is this gif
+                #input_filename = blob.name[start:end:] + "gif" #TODO: why is this gif and do we need to store input file names?
       
                 # Getting ready to read the output of the parsed document - setting up "document"
                 blob_as_string = blob.download_as_string()
@@ -126,6 +125,7 @@ def process_receipt(event, context):
 
                 for entity in document.entities:
                     entity_type = str(entity.type_)
+                    # TODO: this might not be working
                     if "/" in entity_type:
                         entity_type = entity_type.replace("/","_")
                         print("New entity type: ", entity_type)
@@ -176,4 +176,4 @@ def process_receipt(event, context):
        #delete from the input folder
        source_blob.delete()
    else:
-       print('Cannot parse the file type')
+       print('Cannot parse the file type.')
